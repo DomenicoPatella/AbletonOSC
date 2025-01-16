a3737dadef21ae7a8b2fdd186071f9d986b200a9
Domenico Patella <dome2870@gmail.com>
2025-01-16 17:32:39 +0100

    Update view.py

--- abletonosc/view.py
+++ abletonosc/view.py
@@ -22,6 +22,10 @@ class ViewHandler(AbletonOSCHandler):
         
         def get_selected_device(params: Optional[Tuple] = ()):
             return (get_selected_track()[0], list(self.song.view.selected_track.devices).index(self.song.view.selected_track.view.selected_device))
+        
+        def get_follow_song(params: Optional[Tuple] = ()):
+            local=self.song.view.follow_song
+            return(local,)
 
         def set_selected_scene(params: Optional[Tuple] = ()):
             self.song.view.selected_scene = self.song.scenes[params[0]]
@@ -38,19 +42,45 @@ class ViewHandler(AbletonOSCHandler):
             self.song.view.select_device(device)
             return params[0], params[1]
         
+        def set_follow_song(params: Optional[Tuple] = ()):
+            self.song.view.follow_song=params[0]
+            
+
+
+        
        
         self.osc_server.add_handler("/live/view/get/selected_scene", get_selected_scene)
         self.osc_server.add_handler("/live/view/get/selected_track", get_selected_track)
         self.osc_server.add_handler("/live/view/get/selected_clip", get_selected_clip)
         self.osc_server.add_handler("/live/view/get/selected_device", get_selected_device)
+        self.osc_server.add_handler("/live/view/get/follow_song", get_follow_song)
+        
         self.osc_server.add_handler("/live/view/set/selected_scene", set_selected_scene)
         self.osc_server.add_handler("/live/view/set/selected_track", set_selected_track)
         self.osc_server.add_handler("/live/view/set/selected_clip", set_selected_clip)
         self.osc_server.add_handler("/live/view/set/selected_device", set_selected_device)
+        self.osc_server.add_handler("/live/view/set/follow_song", set_follow_song)
+       
 
         #self.osc_server.add_handler("/live/application/get_current_beats_song_time", get_current_beats_song_time)
         
         self.osc_server.add_handler('/live/view/start_listen/selected_scene', partial(self._start_listen, self.song.view, "selected_scene", getter=get_selected_scene))
         self.osc_server.add_handler('/live/view/start_listen/selected_track', partial(self._start_listen, self.song.view, "selected_track", getter=get_selected_track))
+        self.osc_server.add_handler('/live/view/start_listen/follow_song', partial(self._start_listen, self.song.view, "follow_song", getter=get_follow_song))
+        
         self.osc_server.add_handler('/live/view/stop_listen/selected_scene', partial(self._stop_listen, self.song.view, "selected_scene"))
         self.osc_server.add_handler('/live/view/stop_listen/selected_track', partial(self._stop_listen, self.song.view, "selected_track"))
+        self.osc_server.add_handler('/live/view/stop_listen/follow_song', partial(self._stop_listen, self.song.view, "follow_song"))
+
+
+        # Debug purpose only
+        def log_object_info(obj, obj_name="object"):
+         attributes = dir(obj)
+         self.logger.info(f"Oggetto '{obj_name}': elenco metodi e proprietà:")
+         for attr in attributes:
+            try:
+                attr_value = getattr(obj, attr)  # Ottieni il valore dell'attributo
+                self.logger.info(f"  - {attr}: {repr(attr_value)}")
+            except Exception as e:
+                self.logger.info(f"  - {attr}: (non accessibile, errore: {e})")
+
